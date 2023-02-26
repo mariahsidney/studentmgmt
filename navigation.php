44de1ecc8ede947b270d47a7708205abc197ec38
MariahSidney <77188908+mariahsidney@users.noreply.github.com>
2023-02-26 09:27:30 +0800

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>TITLEEEE</title>
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+	
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+	<link rel="stylesheet" type="text/css" href="./style.css">
+ 
+    </head>
+
+<body>
+<nav class = "navbar navbar-expand-lg navbar-dark fixed-top" style = "background-color: #681A1A;">
+        <a class = "navbar-brand" href=#>Web Development</a>
+        <button class = "navbar-toggler" type = "button" data-toggle = "collapse" data-target = "#portfolio-nav" aria-controls = "portfolio-nav" aria-expanded = "false" aria-label = "Toggle navigation">
+            <span class = "navbar-toggler-icon"></span>
+        </button>
+		
+        <div class = "collapse navbar-collapse" id = "portfolio-nav">
+            <ul class="nav navbar-nav mr-auto">
+                <li class ="nav-item">
+                    <a class="nav-link" href="homepage.php">HOME</a>
+                </li>
+                <li class = "nav-item">
+                    <a class="nav-link" href="studentlandingpage.php">STUDENT</a>
+                </li>
+                <li class = "nav-item">
+                    <a class="nav-link" href="#">COURSE</a>
+                </li>
+                <li class = " nav-item">
+                    <a class="nav-link" href="#">INSTRUCTORS</a>
+                </li>
+            
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        GENERATE REPORT
+                    </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">By Number of Participants per Course</a>
+                            <a class="dropdown-item" href="#">By Course conducted by Date Range</a>
+                            <a class="dropdown-item" href="#">By Participants of the Course</a>
+                            <a class="dropdown-item" href="#">By Pool Instructors per Course</a>
+                        </div>
+                </li>
+            </ul>
+
+            <ul class = "nav navbar-nav">
+                <li>
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        PROFILE
+                    </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item">User Name</a>
+                            <a class="dropdown-item">User Type</a>
+                        <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href= #>Log Out</a>
+                        </div>
+                </li> 
+        </div>
+	</nav>
+
+        
+    
+
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+</body>
+</html>
